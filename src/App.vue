2b7d28a1663885d1019f162c1785f0be6d7a99ec
mkaanilhan <kaan.ilhan@proton.me>
2024-07-19 18:43:31 +0300

    added baseUrl

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+
+  data() {
+    return {
+
+      baseUrl: 'https://669a57039ba098ed61ff463c.mockapi.io/api/v1/pizzas'
+    }
+  }
+}
 </script>
 
 <template>
